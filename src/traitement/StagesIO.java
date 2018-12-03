7ad2811d2041046752f1460d5816f561dd162003
OOL <oouali@umanis.com>
2018-12-03 22:54:11 +0100

    integration des donnees stages

--- src/traitement/StagesIO.java
+++ src/traitement/StagesIO.java
@@ -1,14 +1,15 @@
 package traitement;
 
+import contrat.Competence;
+import contrat.Niveau;
 import model.Entreprise;
 import model.Stage;
 
 import java.io.*;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Permet de charger les fichiers contenant les donnees des etudiants et des stages.
@@ -27,16 +28,16 @@ public final class StagesIO {
     /**
      * Représente le chemin du fichier des données stages.
      */
-    private Path stagesFilePath
+    private Path stagesFilePath;
 
 
     public StagesIO(Path etuFilePath, Path stagesFilePath) {
-        this.etuFilePath = null;
-        this.stagesFilePath = null;
-        this.stagesMap = null;
-        this.etusMap = null;
-        this.classesMap = null;
-        this.enseignantsMap = null;
+        this.etuFilePath = etuFilePath;
+        this.stagesFilePath = stagesFilePath;
+        this.stagesMap =  new HashMap<>();
+        this.etusMap = new HashMap<>();
+        this.classesMap = new HashMap<>();
+        this.enseignantsMap = new HashMap<>();
     }
 
     /**
@@ -44,15 +45,161 @@ public final class StagesIO {
      * @throws IOException
      */
     public void chargerDonnees() throws IOException {
+        //Files.lines(etuFilePath).forEach(line ->Stre
+
         BufferedReader in = null;
         try {
-            in = new BufferedReader(new FileReader(String.valueOf(etuFilePath)));
+            in = new BufferedReader(new FileReader(stagesFilePath.toString()));
             String read = null;
             while ((read = in.readLine()) != null) {
 
                 String[] splited = read.split("#");
                 // OOL MODIFICATION ICI
                 Entreprise entreprise = new Entreprise(splited[4]);
+                switch (splited[2]){
+                    case "AMOA" :{
+                        switch (splited[3]){
+                            case "L3":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.AMOA, Niveau.L3,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                            case "M1":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.AMOA, Niveau.M1,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                            case "M2":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.AMOA, Niveau.M2,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                        }
+                    }
+                    break;
+                    case "AMOE" :{
+                        switch (splited[3]){
+                            case "L3":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.AMOE, Niveau.L3,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                            case "M1":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.AMOE, Niveau.M1,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                            case "M2":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.AMOE, Niveau.M2,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                        }
+                    }
+                    break;
+                    case "DONNEES" :{
+                        switch (splited[3]){
+                            case "L3":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.DONNEES, Niveau.L3,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                            case "M1":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.DONNEES, Niveau.M1,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                            case "M2":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.DONNEES, Niveau.M2,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                        }
+                    }
+                    break;
+                    case "MOBILE" :{
+                        switch (splited[3]){
+                            case "L3":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.MOBILE, Niveau.L3,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                            case "M1":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.MOBILE, Niveau.M1,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                            case "M2":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.MOBILE, Niveau.M2,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                        }
+                    }
+                    break;
+                    case "SECURITE" :{
+                        switch (splited[3]){
+                            case "L3":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.SECURITE, Niveau.L3,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                            case "M1":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.SECURITE, Niveau.M1,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                            case "M2":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.SECURITE, Niveau.M2,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                        }
+                    }
+                    break;
+                    case "SYSADMIN" :{
+                        switch (splited[3]){
+                            case "L3":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.SYSADMIN, Niveau.L3,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                            case "M1":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.SYSADMIN, Niveau.M1,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                            case "M2":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.SYSADMIN, Niveau.M2,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                        }
+                    }
+                    break;
+                    case "WEB" :{
+                        switch (splited[3]){
+                            case "L3":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.WEB, Niveau.L3,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                            case "M1":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.WEB, Niveau.M1,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                            case "M2":{
+                                Stage stage = new Stage(splited[0],splited[1], Competence.WEB, Niveau.M2,entreprise);
+                                stagesMap.put(splited[0],stage);
+                            }
+                            break;
+                        }
+                    }
+                    break;
+
+                }
+
                 for (String part : splited) {
 
                     System.out.println(part);
@@ -102,7 +249,9 @@ public final class StagesIO {
      * @return la liste des stages
      */
     public List<contrat.Stage> getStages(){
-        return null;
+        ArrayList result = new ArrayList(Collections.singleton(stagesMap.values().toString()));
+        return result;
+
     }
 
 }
